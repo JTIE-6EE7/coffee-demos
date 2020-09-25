07675b9d4c110720a2a7cee2b97285fabf84033a
JT <foosyou@gmail.com>
2020-09-25 08:42:25 -0500

    csv writer

--- 2-nornir-basics/6-exporting-data.py
+++ 2-nornir-basics/6-exporting-data.py
@@ -6,7 +6,7 @@ This script is used to run a command on network devices with Nornir
 
 from nornir import InitNornir
 from nornir.plugins.tasks.networking import netmiko_send_command
-from openpyxl import Workbook
+import csv
 
 
 def run_command(task):
@@ -22,14 +22,22 @@ def run_command(task):
     task.host["ip_addresses"] = output.result
 
 
-def print_addresses(task):
-    # print inventory hostname
-    print(task.host)
-    # print previously assigned output
+def write_addresses(task):
+    # initialize list with device hostname
+    csv_row = [task.host.name]
+    # iterate over interfaces
     for intf in task.host["ip_addresses"]:
-        print(intf["intf"])
-        print(intf["ipaddr"])
-    print()
+        # append each IP address to the list
+        csv_row.append(intf["ipaddr"])
+    # print CSV row for debugging
+    print(csv_row)
+    
+    # open or create CSV file
+    with open('ip_addresses.csv', mode='a') as ipaddr_file:
+        # initialize CSV writer
+        ipaddr_writer = csv.writer(ipaddr_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        # write row of data to CSV file
+        ipaddr_writer.writerow(csv_row)
 
 
 def main():
@@ -37,21 +45,8 @@ def main():
     nr = InitNornir("config.yaml")
     # run The Norn to run command
     nr.run(task=run_command)
-
-    filename = "hello_world.xlsx"
-
-    workbook = Workbook()
-
-    sheet = workbook.active
-    
-    sheet["A1"] = "hello"
-
-    sheet["B1"] = "world!"
-
-    workbook.save(filename=filename)
-
     # print results for each host
-    nr.run(task=print_addresses)
+    nr.run(task=write_addresses)
     
 
 if __name__ == "__main__":
