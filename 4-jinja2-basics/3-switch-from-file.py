4cb3e4754ece6ba46b131b5d944336b5d7b63b2c
JT <foosyou@gmail.com>
2021-10-20 08:32:14 -0500

    more rename

--- 4-jinja2-basics/3-switch-from-file.py
+++ 4-jinja2-basics/3-switch-from-file.py
@@ -2,11 +2,11 @@ import yaml
 from jinja2 import Template
 
 # Load Jinja2 template from file
-with open("3-router-template.j2") as f:
+with open("3_switch-template.j2") as f:
     j2_template = Template(f.read())
 
 # Load config data from YAML file
-with open("3-router-data.yaml") as f:
+with open("3_switch-data.yaml") as f:
     data = yaml.safe_load(f)
 
 # Render configuration
