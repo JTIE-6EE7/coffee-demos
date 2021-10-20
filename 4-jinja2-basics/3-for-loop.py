55f903d86fa70217ac74b88b4a0f9155838405bf
JT <foosyou@gmail.com>
2021-10-19 21:01:24 -0500

    moar jinja

--- 4-jinja2-basics/3-for-loop.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import Template
-
-j2_template = Template("My favorite numbers: {% for n in numbers %}{{n}} {% endfor %}")
-
-my_output = j2_template.render(numbers=range(1,11))
-
-print()
-print(my_output)
-print()
